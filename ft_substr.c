76583604e7b72555405e8557650a96da4ca66803
Alban Billiette <albillie@c2r2p6.42lehavre.fr>
2024-10-20 05:15:08 +0200

    UPDATE

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/20 03:04:45 by albillie          #+#    #+#             */
-/*   Updated: 2024/10/20 03:06:44 by albillie         ###   ########.fr       */
+/*   Updated: 2024/10/20 05:01:54 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,8 @@
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+	if (!s || start < 0 || len < 0)
+		return (NULL);
+
+
 }

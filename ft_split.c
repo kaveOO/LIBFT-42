f6bdacde313b228ff94c09f21157ee2afeb1d08a
Alban Billiette <albillie@c1r3p3.42lehavre.fr>
2024-10-26 02:31:22 +0200

    UPDATE

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/21 02:00:38 by albillie          #+#    #+#             */
-/*   Updated: 2024/10/24 06:13:02 by albillie         ###   ########.fr       */
+/*   Updated: 2024/10/26 02:30:48 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,37 +38,75 @@ static size_t	ft_count_words(const char *s, char c)
 	}
 	return (count);
 }
-static char	**ft_fill_array(const char *s, char c)
+
+static char	*ft_word_filler(const char *s, char c)
 {
-	char	**array;
-	size_t	*word_len;
+	char	*word;
+	size_t	len;
 	size_t	i;
 
-	while (*s)
+	len = 0;
+	while (s[len] && s[len] != c)
+		len++;
+	word = malloc(len + 1);
+	if (!word)
+		return (NULL);
+	i = 0;
+	while (i < len)
 	{
-		if (*s == c)
-			s++;
-		
+		word[i] = s[i];
+		i++;
 	}
-
-
-
-
+	word[i] = '\0';
+	return (word);
 }
-
+static void	ft_free_array(char **array, size_t i)
+{
+	while (i--)
+	{
+		free(array[i]);
+	}
+	free(array);
+}
 char	**ft_split(char const *s, char c)
 {
-	size_t	words_count;
+	char	**array;
+	size_t	i;
 
-	words_count = malloc(ft_count_words(s, c) + 1);
+	array = malloc((ft_count_words(s, c)+ 1) * sizeof(char *));
+	if (!array)
+		return (NULL);
+	i = 0;
+	while (*s)
+	{
+		while (*s && *s == c)
+			s++;
+		if (*s)
+		{
+			array[i]  = ft_word_filler(s, c);
+			if (!array[i])
+				return (ft_free_array(array, i), NULL);
+			i++;
+		}
+		while (*s && *s !=c)
+			s++;
+	}
+	array[i] = NULL;
+	return (array);
 }
 
-
-
-int main()
+/* int main()
 {
-	char *string = "je suis un test";
+	char **result;
+	size_t i = 0;
 
-	int result = ft_count_words(string, 'T');
-	printf("%d\n", result);
-}
+	result = ft_split("hello!zzzzouizzzz", 'z');
+	if (!result)
+		return (1);
+
+	while (result[i])
+	{
+		printf("%s\n", result[i]);
+		i++;
+	}
+} */

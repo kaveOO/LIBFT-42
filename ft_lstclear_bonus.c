ea9afc306b8ecac5ff414618760d9cb3b003a46e
kaveO <alban.billiette@gmail.com>
2024-12-16 07:08:50 +0100

    J"adore les commits

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone_bonus.c                               :+:      :+:    :+:   */
+/*   ft_lstclear_bonus.c                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: albillie <albillie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/14 01:42:04 by albillie          #+#    #+#             */
-/*   Updated: 2024/12/16 06:22:26 by albillie         ###   ########.fr       */
+/*   Created: 2024/12/16 06:26:10 by albillie          #+#    #+#             */
+/*   Updated: 2024/12/16 07:07:26 by albillie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,31 @@
 
 void	del(void *element)
 {
-	element = NULL;
-	free(element);
+	(void) element;
+}
+void ft_lstclear(t_list **lst, void (*del)(void*))
+{
+	(void) del;
+	t_list *temp;
+
+	temp = (*lst);
+	while (temp)
+	{
+		printf("%s\n", (char *) temp->content);
+		temp = temp->next;
+	}
 }
 
-void	ft_lstdelone(t_list *lst, void (*del) (void*))
+int main()
 {
-	del(lst->content);
-	free(lst);
-}
+	t_list	*list;
 
-/* int main()
-{
-	t_list *list;
-
-	list = ft_lstnew("kaveO");
+	list = ft_lstnew("test");
 	ft_lstadd_front(&list, ft_lstnew("albillie"));
-	// ft_lstadd_front(&list, ft_lstnew("gbruscan"));
-	// ft_lstadd_front(&list, ft_lstnew("j'adore les chats"));
+	ft_lstadd_front(&list, ft_lstnew("damdam"));
+	ft_lstadd_front(&list, ft_lstnew("anoteris"));
+	ft_lstadd_front(&list, ft_lstnew("gbsruscan"));
 
+	ft_lstclear(&list, del);
 
-	ft_lstdelone(list, del);
-	// list = list->next;
-	printf("%s\n", (char *) list->next);
-	// ft_lstdelone(list, del);
-} */
+}
